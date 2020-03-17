8e4a13eba34ff3a1eb9ffbb333bf630cff8fc6cc
ern0ld <epa.suvila@gmail.com>
2020-03-17 13:49:03 +0200

    stackoverflow ftw, form implemented using formspree

--- mail.js
+++ mail.js
@@ -1,22 +1,42 @@
-function checkFields() {
-    missinginfo = "";
-    if (document.form.name.value == "") {
-    missinginfo += "\n - Your Name";
-    }
-    if ((document.form.email.value == "") || 
-    (document.form.email.value.indexOf('@') == -1) || 
-    (document.form.email.value.indexOf('.') == -1)) {
-    missinginfo += "\n - Your Email";
-    }
-    if(document.form.message.value == "") {
-    missinginfo += "\n - Your Message.";
-    }
-    if (missinginfo != "") {
-    missinginfo ="\n" +
-    "You did not (correctly) fill in the following field(s):\n" +
-    missinginfo + "\n" + "\nPlease make the necessary corrections.";
-    alert(missinginfo);
-    return false;
-    }
-    else return true;
+function validateContactForm() {
+        var valid = true;
+
+        $(".info").html("");
+        $(".input-field").css('border', '#e0dfdf 1px solid');
+        var userName = $("#userName").val();
+        var userEmail = $("#userEmail").val();
+        var subject = $("#subject").val();
+        var content = $("#content").val();
+        
+        if (userName == "") {
+            $("#userName-info").html("*");
+            $("#userName").css('border', '#e66262 1px solid');
+            valid = false;
+        }
+        if (userEmail == "") {
+            $("#userEmail-info").html("*");
+            $("#userEmail").css('border', '#e66262 1px solid');
+            valid = false;
+        }
+        if (!userEmail.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
+        {
+            $("#userEmail-info").html("Invalid Email Address.");
+            $("#userEmail").css('border', '#e66262 1px solid');
+            valid = false;
+        }
+
+      /*  if (subject == "") {
+            $("#subject-info").html("*");
+            $("#subject").css('border', '#e66262 1px solid');
+            valid = false;
+        }*/
+        if (content == "") {
+            $("#userMessage-info").html("*");
+            $("#content").css('border', '#e66262 1px solid');
+            valid = false;
+        }
+ 
+          return valid;
+        
+       
     }
